9a0bf81b7795d94423cf5530b007222cf4a0215b
git_xuanxuan <779247257@qq.com>
2020-04-08 17:30:54 +0800

    项目测试 html解析model   增加 README说明文档

---
+++ src/main/java/com/xuan/forum/HelloTest.java
@@ -0,0 +1,21 @@
+package com.xuan.forum;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @创建人： xuanxuan
+ * @创建时间： 2020/4/8
+ * @描述：
+ */
+@Controller
+public class HelloTest {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name" ,required = false ,defaultValue = "空的默认值") String name, Model model){
+        System.out.println("接受到的name："+name);
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
